baefdc3d1c8e0a3728a14d8111d2df7b259917ff
mitstack <deepabhawnani@yahoo.com>
2022-05-12 17:26:23 -0400

    res-usability fix

--- tests/GETMethodTests.js
+++ tests/GETMethodTests.js
@@ -6,26 +6,24 @@ const request = require("supertest")(testdata.apiendpoint);
 const assert = require("chai").assert;
 var should = require("chai").should();
 
-
-
 /*TODO : Add Callback function ForEach loop to add test(s) data*/
 
-function GetCallResponse() {
+GetValidTestResponse = () => {
+  return request.get(testdata.getvalidtest).set(testdata.commonHeaders);
+}
 
-   return request
-      .get(testdata.getvalidtest)
-      .set(testdata.commonHeaders)
+GetInvalidTestResponse = () => {
+  return request.get(testdata.getinvalidtest).set(testdata.commonHeaders);
+}
 
+GetMultiplePostTestResponse = () => {
+  return request.get(testdata.getrooturl).set(testdata.commonHeaders);
 }
 
 describe("API GET Tests", () => {
   // Test 1 : Make a GET request to the single posts route and do schema validation
-  it("GET - Response Validation for Single Posts",  () => {
-    
-  
-    
-    
-       GetCallResponseResult =  GetCallResponse()
+  it("GET - Response Validation for Single Posts", () => {
+    return ResponseResult = GetValidTestResponse()
       .expect(200)
       .then((res) => {
         //GET Response Schema Validation
@@ -39,9 +37,8 @@ describe("API GET Tests", () => {
   });
 
   // Test 2 : Make a invalid GET request and verify the response payload is empty
-  it("GET - Invalid Request Error Handling",  () => {
-    return request
-      .get(testdata.getinvalidtest)
+  it("GET - Invalid Request Error Handling", () => {
+    return ResponseResult = GetInvalidTestResponse()
       .expect(400)
       .then((res) => {
         //assertion response is empty when invalid id is provided
@@ -52,9 +49,8 @@ describe("API GET Tests", () => {
   /*TODO : Schema Validation - contract testing */
 
   // Test 3 : Make a GET request to the multiple posts route and
-  it("GET - Response Validation for Multiple posts",  () => {
-    return request
-    .get(testdata.getrooturl)
+  it("GET - Response Validation for Multiple posts", () => {
+    return ResponseResult = GetMultiplePostTestResponse()
       .expect(200)
       .then((res) => {
         //assertion response is not empty and 100 posts exist
