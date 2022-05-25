b560e31d17facf517c4a74b5b5900cce29b906d4
mitstack <deepabhawnani@yahoo.com>
2022-05-25 15:41:44 -0400

    end to end test

--- tests/EndToEndTests.js
+++ tests/EndToEndTests.js
@@ -1,7 +1,9 @@
+const { testdata } = require("./data.js");
+const { responsedata } = require("./data.js");
 const { Suite, Test } = require("mocha");
-
+const { expect } = require("chai");
 const request = require("supertest")(testdata.apiendpoint);
-const assert = require("chai").assert;
+const { assert } = require("chai");
 
 /* IN PROGRESS
 
@@ -11,45 +13,43 @@ Step -2  - updating the posts by calling the PUT route
 Step -3  - listing the posts by calling the GET route 
 Step -4  - create the posts by calling the DELETE route 
 Step -5  - Listing the posts by calling the GET route and make sure deleted post is not returned
-
+//https://visionmedia.github.io/superagent/
 */
 
 describe("End To End Tests", () => {
   //Posting the data
 
-  it("Updating the recently added User", (done) => {
+  it("Delete the recently updated post", () => {
     var requestbody = {
       title: "foo",
       body: "bar",
       userId: 1,
     };
 
-    request
+    return request
       .post("/posts")
       .send(requestbody)
-      .expect(200)
-      .then((response) => {
+      .expect(201)
+      .then((response1) => {
         // verify post response
-        assert.isNotEmpty(response.body);
-      });
-
-    request
-      .put("/posts/1")
-      .send(requestbody)
-      .expect(200)
-      .expect(function (response) {
-        // updating the recently added post
-        assert.isNotEmpty(response.body);
-      });
-
-    request
+        assert.isNotEmpty(response1.body);
+       // console.log("response of the post call", response1.body);
+       
+      return request
       .get("/posts/1")
-      .send(requestbody)
       .expect(200)
-      .expect(function (response) {
-        // updating the recently added post - bug in the api (no real time changes)
-        // assert.equal(response.body, requestbody);
+      .then(response2 => {
+        assert.isNotEmpty(response2.body);
+        console.log("response of the post call earlier", response1.body)
+        console.log("response of the get call", response2.body)
+        
+
       })
-      .end(done);
+      });
+
+   
+
+    
+      
   });
 });
